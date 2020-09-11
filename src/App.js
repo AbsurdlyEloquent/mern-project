50c0a8c39a9d5fba1d136286ed415ff5c40d5a90
Ethan Henderson <ethan.henderson397@gmail.com>
2020-09-11 15:50:01 -0400

    added new components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 /* Dependencies */
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom'
 /* Stylesheets */
 import './App.css';
 /* Components */
-import Sidebar from './components/Sidebar'
+import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Sidebar />
+        <Switch>
+          <Route exact path="/" component="Home" />
+          <Route path="/new" component="Create"/>
+          <Route path="/project" component="Project" />
+          <Route path="/myprojects" component="UserProjects" />
+        </Switch>
       </div>
     </Router>
   );
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,27 @@
+/* dependencies */
+import React from 'react'
+import { Link } from 'react-router-dom'
+/* stylesheets */
+import './Sidebar.css'
+
+class Sidebar extends React.Component {
+  render() {
+    return (
+      <div>
+        <aside className="sidebar">
+          <h3>My Projects API</h3>
+          <nav>
+            <ul>
+              <lh>Navigation</lh>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/new">Create</Link></li>
+              <li><Link to="/myprojects">My Projects</Link></li>
+            </ul>
+          </nav>
+        </aside>
+      </div>
+    )
+  }
+}
+
+export default Sidebar
