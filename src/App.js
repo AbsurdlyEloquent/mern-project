34530dab2d8033439ba86010b172f727452df166
Ethan Henderson <ethan.henderson397@gmail.com>
2020-09-11 14:22:14 -0400

    installed react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          
+        </header>
+      </div>
+    </Router>
   );
 }
 
