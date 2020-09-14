5a48bbc07dbe35e78790484ad10c3523a7832450
Ethan Henderson <ethan.henderson397@gmail.com>
2020-09-14 17:31:00 -0400

    added change handlers

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,66 +6,53 @@ class Form extends React.Component {
 
     if (this.props.project) {
       this.state = {
-        project: this.props.project
+        project: this.props.project,
+        newProject: {}
       }
     } else {
       this.state = {
-        project: null
+        project: {},
+        newProject: {}
       }
     }
+    this.changeHandler = this.changeHandler.bind(this)
+    this.resetHandler = this.resetHandler.bind(this)
   }
   render() {
-    if (this.state.project) {
     return (
-      <form onSubmit={this.putHandler}>
+      <form onReset={this.resetHandler} >
         {/* Forms are so hard to read, its just a bunch of label, input pairs */}
-            <label htmlFor="name">Name:</label>
-          <input id="name" type="text" defaultValue={this.state.project.name} />
-            <label htmlFor="description">Description:</label>
-          <input id="description" type="text" defaultValue={this.state.project.description} />
-            <label htmlFor="url">URL:</label>
-          <input id="url" type="text" defaultValue={this.state.project.url} />
-            <label htmlFor="homepage">Homepage:</label>
-          <input id="homepage" type="text" defaultValue={this.state.project.homepage} />
-            <label htmlFor="language">Language:</label>
-          <input id="language" type="text" defaultValue={this.state.project.language} />
-            <label htmlFor="framwork">Framework:</label>
-          <input id="framwork" type="text" defaultValue={this.state.project.framwork} />
-            <label htmlFor="private">Private:</label>
-          <input id="true" className="private" name="private" type="radio" value="true" />
-            <label htmlFor="true">True</label>
-          <input id="false" className="private" name="private" type="radio" value="false" checked readOnly/>
-            <label htmlFor="false">False</label>
-          <input type="submit" />
-          <input type="reset" />
-        </form>
-      )
-    } else {
-      return (
-        <form onSubmit={this.putHandler}>
-          {/* Forms are so hard to read, its just a bunch of label, input pairs */}
-              <label htmlFor="name">Name:</label>
-            <input id="name" type="text" />
-              <label htmlFor="description">Description:</label>
-            <input id="description" type="text" />
-              <label htmlFor="url">URL:</label>
-            <input id="url" type="text" />
-              <label htmlFor="homepage">Homepage:</label>
-            <input id="homepage" type="text" />
-              <label htmlFor="language">Language:</label>
-            <input id="language" type="text" />
-              <label htmlFor="framwork">Framework:</label>
-            <input id="framwork" type="text" />
-              <label htmlFor="private">Private:</label>
-            <input id="true" className="private" name="private" type="radio" value="true" />
-              <label htmlFor="true">True</label>
-            <input id="false" className="private" name="private" type="radio" value="false" checked readOnly/>
-              <label htmlFor="false">False</label>
-            <input type="submit" />
-            <input type="reset" />
-          </form>
-      )
-    }
+          <label htmlFor="name">Name:</label>
+        <input id="name" type="text" defaultValue={this.state.project.name} onChange={this.changeHandler} />
+          <label htmlFor="description">Description:</label>
+        <input id="description" type="text" defaultValue={this.state.project.description} onChange={this.changeHandler} />
+          <label htmlFor="url">URL:</label>
+        <input id="url" type="text" defaultValue={this.state.project.url} onChange={this.changeHandler} />
+          <label htmlFor="homepage">Homepage:</label>
+        <input id="homepage" type="text" defaultValue={this.state.project.homepage} onChange={this.changeHandler} />
+          <label htmlFor="language">Language:</label>
+        <input id="language" type="text" defaultValue={this.state.project.language} onChange={this.changeHandler} />
+          <label htmlFor="framwork">Framework:</label>
+        <input id="framwork" type="text" defaultValue={this.state.project.framwork} onChange={this.changeHandler} />
+          <label htmlFor="private">Private:</label>
+        <input id="true" className="private" name="private" type="radio" value="true" />
+          <label htmlFor="true">True</label>
+        <input id="false" className="private" name="private" type="radio" value="false" checked readOnly />
+          <label htmlFor="false">False</label>
+        <input type="submit" />
+        <input type="reset" />
+      </form>
+    )
+  }
+  changeHandler(e) {
+    let newProject = this.state.newProject
+    newProject[`${e.target.id}`] = e.target.value
+    this.setState({ newProject: newProject})
+    console.log(this.state.newProject)
+  }
+  resetHandler() {
+    this.setState({ newProject: {} })
+    console.log(this.state.newProject)
   }
 }
  export default Form
