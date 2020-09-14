ea8d9a9fd87d99030e662aac58c96873d0184999
Ethan Henderson <ethan.henderson397@gmail.com>
2020-09-14 11:38:58 -0400

    passed props to <Home />

--- src/App.js
+++ src/App.js
@@ -13,23 +13,31 @@ import UserProjects from './components/UserProjects/UserProjects'
 const axios = require('axios').default
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      projects: null
+    }
+  }
   async componentDidMount() {
     try {
       // !! CHANGE TO 'https://projects-absurdlyeloquent.herokuapp.com/projects' BEFORE DEPLOY !!
       const projects = await axios.get('http://lvh.me:3002/projects')
-      console.log(projects)
+      this.setState({ projects: projects.data })
     }
     catch (err) {
       console.error(err)
     }
   }
   render() {
+    if (this.state.projects) {
     return (
       <Router>
         <div className="App">
           <Sidebar />
           <Switch>
-            <Route exact path="/" component={Home} />
+            <Route exact path="/" render={()=> (<Home projects={this.state.projects}/>) } />
             <Route path="/new" component={Create} />
             <Route path="/project" component={Project} />
             <Route path="/myprojects" component={UserProjects} />
@@ -37,6 +45,15 @@ class App extends React.Component {
         </div>
       </Router>
     );
+  } else {
+    return (
+      <Router>
+        <div> className="App">
+          <Sidebar />
+        </div>
+      </Router>
+    )
+  }
   }
 }
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,33 @@
 import React from 'react'
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      projects: this.props.projects
+    }
+  }
   render() {
     return (
-      <div>
-
-      </div>
+      <main className="home">
+        <div className="accordion">
+          <ul>
+          { /* JS */
+            this.state.projects.map((item, i) => {
+              return (
+                <li key={i} index={i}><button onClick={this.expand}>{item.name}</button></li>
+              )
+            })
+            /* JS */ }
+          </ul>
+        </div>
+      </main>
     )
+  }
+  expand(e) {
+    let i = e.target.parentElement.getAttribute('index')
+    console.log(i)
   }
 }
 
