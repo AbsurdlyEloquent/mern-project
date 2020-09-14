240a5fdfb8e334f90ef1620477b738dfa9a16b9e
Ethan Henderson <ethan.henderson397@gmail.com>
2020-09-14 16:23:41 -0400

    wrote form, adding event listener

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -26,15 +26,31 @@ class Project extends React.Component {
     // without this if,
     // the component will throw an error before the promise is finished
     if (this.state.project) {
+      let project = this.state.project
       return (
         <div className="project-edit">
-          <form>
-            <label htmlFor="name">Name:</label>
-            <input id="name" type="text" defaultValue={this.state.project.name} />
-            <label htmlFor="desc">Description:</label>
-            <input id="desc" type="text" defaultValue={this.state.project.description} />
-            <label htmlFor="url">URL:</label>
-            <input id="url" type="text" defaultValue={this.state.project.url} />
+          <h4>Edit Project:</h4>
+          <form onSubmit={this.putHandler}>
+          {/* Forms are so hard to read, its just a bunch of label, input pairs */}
+              <label htmlFor="name">Name:</label>
+            <input id="name" type="text" defaultValue={project.name} />
+              <label htmlFor="description">Description:</label>
+            <input id="description" type="text" defaultValue={project.description} />
+              <label htmlFor="url">URL:</label>
+            <input id="url" type="text" defaultValue={project.url} />
+              <label htmlFor="homepage">Homepage:</label>
+            <input id="homepage" type="text" defaultValue={project.homepage} />
+              <label htmlFor="language">Language:</label>
+            <input id="language" type="text" defaultValue={project.language} />
+              <label htmlFor="framwork">Framework:</label>
+            <input id="framwork" type="text" defaultValue={project.framwork} />
+              <label htmlFor="private">Private:</label>
+            <input id="true" className="private" name="private" type="radio" value="true" />
+              <label htmlFor="true">True</label>
+            <input id="false" className="private" name="private" type="radio" value="false" checked readOnly/>
+              <label htmlFor="false">False</label>
+            <input type="submit" />
+            <input type="reset" />
           </form>
         </div>
       )
@@ -43,6 +59,9 @@ class Project extends React.Component {
         <div></div>
       )
     }
+  }
+  putHandler(e) {
+    console.log(e.target.elements[0].id+`: `+e.target.elements[0].value)
   }
 }
 
